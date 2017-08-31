864cfe96d84f0d20528350a5bb51cc71a7d090a5
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-09-01 08:37:20 +1000

    Add serde dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,12 @@ extern crate hyper;
 extern crate futures;
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate serde;
+
+#[cfg(test)]
+#[macro_use]
+extern crate serde_derive;
 
 mod middleware;
 mod router;
