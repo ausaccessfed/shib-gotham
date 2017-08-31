7234586e7be69f3709d28ad112f9cf55365eebf6
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-08-31 13:10:13 +1000

    Add skeleton of middleware/router setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+//! Shibboleth SP authentication plugin for Gotham web applications
+
+extern crate gotham;
+#[macro_use]
+extern crate gotham_derive;
+extern crate hyper;
+extern crate futures;
+#[macro_use]
+extern crate log;
+
+mod middleware;
+mod router;
+
+pub use middleware::*;
+pub use router::*;
---
+++ src/middleware.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::marker::PhantomData;
+
+use hyper::Request;
+
+use gotham::state::State;
+use gotham::handler::HandlerFuture;
+use gotham::middleware::{NewMiddleware, Middleware};
+
+trait AttributesTypePhantom<T>: Send + Sync
+where
+    T: Send
+{
+}
+
+/// Gotham middleware for receiving Shibboleth attributes and mapping them into a provided type.
+pub struct Shibbleware<T> {
+    phantom: PhantomData<AttributesTypePhantom<T>>,
+}
+
+impl<T> Clone for Shibbleware<T> {
+    fn clone(&self) -> Self {
+        Shibbleware { phantom: PhantomData }
+    }
+}
+
+impl<T> NewMiddleware for Shibbleware<T> {
+    type Instance = Self;
+
+    fn new_middleware(&self) -> io::Result<Self::Instance> {
+        Ok(self.clone())
+    }
+}
+
+impl<T> Middleware for Shibbleware<T> {
+    fn call<Chain>(self, state: State, request: Request, chain: Chain) -> Box<HandlerFuture>
+    where
+        Chain: FnOnce(State, Request) -> Box<HandlerFuture>,
+    {
+        chain(state, request)
+    }
+}
